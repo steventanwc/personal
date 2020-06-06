0df49c1b9e1139fa249b0b497621ff6445a302fd
steventanwc <steven@mggsoftware.com>
2020-06-06 14:19:09 +0800

    Update Index.vue

--- hello/src/pages/Index.vue
+++ hello/src/pages/Index.vue
@@ -1,5 +1,6 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-center column">
+    HELLO WORLD
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-full.svg"
